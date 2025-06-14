20ac881da8c8641fc28aa3b6609003779fe7c412
KaleChips22 <jonasgarmon@gmail.com>
2025-06-14 01:34:59 -0400

    temporarily disable models taht cost me money for sharing purposes

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,16 +1,16 @@
 const models = [
-  {
-    name: "GPT 4o",
-    id: "openai/gpt-4o",
-    description: "The latest model from OpenAI",
-    icon: ""
-  },
-  {
-    name: "GPT 4o mini",
-    id: "openai/gpt-4o-mini",
-    description: "The mini model from OpenAI",
-    icon: ""
-  },
+  // {
+  //   name: "GPT 4o",
+  //   id: "openai/gpt-4o",
+  //   description: "The latest model from OpenAI",
+  //   icon: ""
+  // },
+  // {
+  //   name: "GPT 4o mini",
+  //   id: "openai/gpt-4o-mini",
+  //   description: "The mini model from OpenAI",
+  //   icon: ""
+  // },
   {
     name: "Deepseek Chat v3",
     id: "deepseek/deepseek-chat-v3-0324:free",
