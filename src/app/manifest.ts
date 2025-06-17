ee9d0c59c59708b6be7e8d316f2d865d4a1dd523
KaleChips22 <jonasgarmon@gmail.com>
2025-06-17 00:33:53 -0400

    the color wasn't even right

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,8 +7,8 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'Open3 Chat is a chatbot that uses the OpenAI API to answer questions and help you with your tasks.',
     start_url: '/',
     display: 'standalone',
-    theme_color: '#262626',
-    background_color: '#262626',
+    theme_color: '#0a0a0a',
+    background_color: '#0a0a0a',
     icons: [
       {
         src: '/favicon.ico',
